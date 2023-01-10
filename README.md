d576e106d0bb8d96601bac839fef4fca0ee8e4ea
Frédéric BARRAU <frederic.barrau@zenika.com>
2023-01-10 16:20:17 +0100

    ADD cloud function for jira checks

--- README.md
+++ README.md
@@ -3,11 +3,96 @@
 Cloud function used as a Github Webhook.
 Will check if a PR name is properly linked to a jira ticket.
 
+The current document is for the cloud function hosted in Google Cloud that will be called from Github in order to validate the branch names in project.
+
+A proper branch name must follow the jira convention, and contain an issue name.
+
 ## Usage
 
+Locally:
 
-## Local testing
+```console
+$ functions_framework --target=jira_github_pr_check
+Runs on port localhost:8080
+```
 
-## Tests
+In Cloud Run, using gcloud for deployment (test only):
 
-## Contact
+```console
+$ gcloud deploy github-jira-webhook --source . --runtime python310 --entry-point jira_github_pr_check
+...
+```
+
+## Configuration
+
+The cloud function grabs the configuration secrets from environment variables.
+These environment variables contain secrets, and should be provided to the cloud function during deployment (see the dedicated terraform code).
+
+For local development, a `dev/.env` file can be created (ignored by git) in order to add these configurations and secrets, see below.
+
+## Development
+
+**VSCode:** for convenience, development / debugging of this cloud function should be done in a dedicated vscode workspace containing the current source folder **only**. The root folder of the cloud function sources contains a vscode configuration file (`.vscode/launch.json`) which, when in a dedicated workspace, provides the configuration for using the debugger of the [python](https://marketplace.visualstudio.com/items?itemName=ms-python.python) extension.
+
+
+### Dependency management
+
+This project uses [poetry](https://python-poetry.org/) for managing the the project dependencies.
+
+Generate the `requirements.txt` for the google cloud builder with the following command once a new dependency has been added / removed:
+
+```console
+$ poetry export --without-hashes --format=requirements.txt > requirements.txt
+```
+
+### Local development
+
+#### Configure the dev/.env file
+
+```ini
+# dev/.env file
+JIRA_DOMAIN=altirnao.atlassian.net
+# See https://docs.python.org/3/library/logging.html#logging-levels
+LOG_LEVEL=10
+JIRA_EMAIL=frederic.barrau@altirnao.com
+# See gcloud secret:
+# List the versions:
+# gcloud secrets versions list jira-github-pr-check-user-token
+# Get the value for the latest version X:
+# gcloud secrets versions access X --secret jira-github-pr-check-user-token
+JIRA_TOKEN=< see google cloud secrets> 
+```
+
+#### Running the cloud function locally
+
+```console
+$ poetry shell
+Spawning shell within /Users/fredericbarrau/Library/Caches/pypoetry/virtualenvs/jira-github-pr-check-python-I_CHN4P6-py3.10
+$ poetry install
+Installing dependencies from lock file
+
+No dependencies to install or update
+$ functions_framework --target=jira_github_pr_check
+...
+```
+
+
+#### Exposing the cloud function with ngrok
+
+Ngrok in a tool for providing a public access of a local development service. 
+
+```console
+
+```
+
+### Local debugging
+
+As mentionned above, the current folder contains a `.vscode/launch.json` file which, when used in a dedicated workspace, provides the configuration for the debugger of the [python](https://marketplace.visualstudio.com/items?itemName=ms-python.python) vscode extension.
+
+
+## Notes
+
+### Refs
+
+* `ref/heads/main`
+
--- main.py
+++ main.py
@@ -1,34 +1,109 @@
+from dotenv import load_dotenv
+from functions_framework import logging
 import functions_framework
-from logging import DEBUG
+from jira import JIRA
+import os
+import re
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+
+
+class NotJiraIssueException(Exception):
+    """
+    Raised when a Github branch is not referenced as a Jira issue
+    """
+
+    pass
+
+
+def get_jira_issue_from_branch_name(branch_name: str) -> str:
+    issue_re = "^([a-z,A-Z]+-[0-9]+).*$"
+    match = re.match(issue_re, branch_name)
+    return match[1] if match is not None else match
+
+
+def is_jira_issue(config: dict, issue_id: str) -> bool:
+    result = True
+    try:
+        jira = JIRA(
+            server="https://" + config["jira_domain"],
+            basic_auth=(config["jira_email"], config["jira_token"]),
+        )
+        log.debug(f"looking of issue '{issue_id}' in Jira..")
+        # will trigger an exception when the issue is not found
+        jira.issue(issue_id)
+        log.debug(f"{issue_id} found in Jira")
+    except Exception as e:
+        log.debug(e)
+        result = False
+    return result
+
+
+def get_branch_name(git_ref: str) -> str | None:
+    branch_name_re = "^refs/heads/(.*)$"
+    match = re.match(branch_name_re, git_ref)
+    return match[1] if match is not None else match
+
+
+def get_config() -> dict:
+    # Load the local dotenv, if present (for dev env mostly)
+    load_dotenv(dotenv_path="dev/.env")
+    config = {}
+    config["jira_domain"] = os.getenv("JIRA_DOMAIN")
+    config["log_level"] = (
+        int(os.getenv("LOG_LEVEL"))
+        if os.getenv("LOG_LEVEL") is not None
+        else logging.INFO
+    )
+    config["jira_email"] = os.getenv("JIRA_EMAIL")
+    config["jira_token"] = os.getenv("JIRA_TOKEN")
+    return config
 
 
 # Register an HTTP function with the Functions Framework
 @functions_framework.http
-def my_http_function(request):
-    # Your code here
-
-    # Return an HTTP response
-    return "OK"
-
-
-if __name__ == "main":
-    from flask import Flask
-
-    app = Flask(__name__)
-    app.logger.setLevel(DEBUG)
-
-    @app.route("/payload", methods=["POST"])
-    def payload(request):
-        app.logger.debug("Request received:")
-        app.logger.debug(request)
-        return {}
-
-    @app.errorhandler(404)
-    def not_found(error):
-        app.logger.debug(error)
-        response = {
-            "code"       : error.code,
-            "description": error.description,
-            "name"       : error.name
-        }
-        return response
+def jira_github_pr_check(request):
+    code = 200
+    result = "OK"
+    config = get_config()
+    # Set the log level
+    log.setLevel(config["log_level"])
+    try:
+        payload = request.get_json()
+        push_branch = payload["ref"]
+        branch_name = get_branch_name(git_ref=push_branch)
+        if branch_name is None:
+            log.debug("branch name not found in the github payload")
+            raise ValueError(
+                "Github payload is not a proper JSON: github webhook must send a JSON payload in application/json MIME type. Review the webhook settings"
+            )
+        log.debug(f"branch name found: {branch_name}")
+        issue_id = get_jira_issue_from_branch_name(branch_name)
+        log.debug(f"issue id found ({issue_id}) in branch name {branch_name}")
+        if issue_id is None:
+            log.error(
+                f"branch name {branch_name} does not fit the JIRA branch name requirements"
+            )
+            raise NotJiraIssueException(
+                f"branch name {branch_name} does not fit the JIRA branch name requirements"
+            )
+        if not is_jira_issue(config=config, issue_id=issue_id):
+            log.debug(f"issue ID {issue_id} is not a jira issue")
+            raise NotJiraIssueException(
+                f"branch name {branch_name} does not reference a JIRA issue. Issue Id ({issue_id}) not found in JIRA."
+            )
+    except NotJiraIssueException as e:
+        log.error(e)
+        result = {"message": str(e)}
+        code = 404
+    except Exception as e:
+        log.error(e)
+        result = {"message": str(e)}
+        code = 400
+    log.info(f"branch name ({branch_name}) references a found Jira issue ({issue_id})")
+    return result, code
