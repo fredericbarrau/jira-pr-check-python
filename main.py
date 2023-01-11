352c649bf2548dcf081299aafc5d6d424d2402c9
Frédéric BARRAU <frederic.barrau@zenika.com>
2023-01-11 14:51:52 +0100

    ADD github commit status

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from dotenv import load_dotenv
 from functions_framework import logging
-import functions_framework
+from github import Github
 from jira import JIRA
+import functions_framework
 import os
 import regex
-
 
 log = logging.getLogger(__name__)
 logging.basicConfig(
@@ -67,12 +67,38 @@ def get_config() -> dict:
     return config
 
 
+def push_github_commit_status(commit_status: dict) -> bool:
+    g = Github(
+        base_url="https://github.com/api/v3",
+        login_or_token=commit_status["github_token"],
+    )
+    repo = g.get_repo(commit_status["repository_name"])
+    repo.get_commit(sha=commit_status["commit_sha"]).create_status(
+        state=commit_status["status"],
+        target_url=commit_status["callback_url"],
+        description=commit_status["message"],
+        context="github-pr/branch-name-contains-jira-issue",
+    )
+
+
 # Register an HTTP function with the Functions Framework
 @functions_framework.http
 def jira_github_pr_check(request):
     code = 200
     result = "OK"
+
     config = get_config()
+
+    # Initalize commit status
+    github_commit_status = {
+        "commit_sha": None,
+        "repository_name": None,
+        "github_token": config["GITHUB_TOKEN"],
+        "message": "failed to check if branch name has a jira issue (probably not)",
+        "callback_url": request.url,
+        "status": "failure",
+    }
+
     # Set the log level
     log.setLevel(config["log_level"])
     log.info(f"log level: {log.level}")
@@ -85,6 +111,13 @@ def jira_github_pr_check(request):
                 "Github payload is not a proper JSON: github webhook must send a JSON payload in application/json MIME type. Review the webhook settings"
             )
         log.debug(f"branch name found: {branch_name}")
+
+        # amend github commit status
+        github_commit_status["commit_sha"] = payload["pull_request"]["head"]["sha"]
+        github_commit_status["repository_name"] = payload["pull_request"]["head"][
+            "repo"
+        ]["full_name"]
+
         issue_id = get_jira_issue_from_branch_name(branch_name)
         log.debug(f"issue id found ({issue_id}) in branch name {branch_name}")
         if issue_id is None:
@@ -102,12 +135,31 @@ def jira_github_pr_check(request):
         log.info(
             f"branch name ({branch_name}) references a found Jira issue ({issue_id})"
         )
+        # We dit it, we dit it ! (c) Dora
+        github_commit_status[
+            "message"
+        ] = f"branch name ({branch_name}) references a found Jira issue ({issue_id})"
+        github_commit_status["status"] = "success"
+
     except NotJiraIssueException as e:
+        github_commit_status["message"] = e
+
         log.error(e)
         result = {"message": str(e)}
         code = 404
     except Exception as e:
+        github_commit_status["message"] = e
+
         log.error(e)
         result = {"message": str(e)}
         code = 400
+
+    try:
+        # Send github commit status
+        push_github_commit_status(github_commit_status)
+    except Exception as e:
+        log.error(e)
+        result = {"message": str(e)}
+        code = 500
+
     return result, code
